0f1295b13cb9d7f1a15bf86016f752bca56562fe
thomasrosales <tomasjoaquinrosales@gmail.com>
2020-02-23 16:02:55 -0300

    Components

--- section_1/clitest/src/app/app.module.ts
+++ section_1/clitest/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MainpageComponent } from './pages/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule

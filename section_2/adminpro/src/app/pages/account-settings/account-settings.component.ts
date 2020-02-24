1b81b364d03719c9391a74c55567ed368fe40413
thomasrosales <tomasjoaquinrosales@gmail.com>
2020-02-24 18:36:58 -0300

    Check Mark and Click Listeners

--- section_2/adminpro/src/app/pages/account-settings/account-settings.component.ts
+++ section_2/adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor( @Inject(DOCUMENT) private _document ) { }
 
   ngOnInit(): void {
+  }
+
+  changeColor(colorTheme: string, link: ElementRef){
+    this.checkMark(link);
+    let url = `assets/css/colors/${colorTheme}.css`;
+    this._document.getElementById('theme').setAttribute('href', url);
+  }
+
+  checkMark(link: any){
+    let selector: any = document.getElementsByClassName('selector');
+    for(let element of selector){
+      element.classList.remove('working');
+    }
+    link.classList.add('working');
   }
 
 }

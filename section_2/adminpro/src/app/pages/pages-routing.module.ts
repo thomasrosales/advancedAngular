a0007d3bba4bd92a3c5525e1757665020c08ba83
thomasrosales <tomasjoaquinrosales@gmail.com>
2020-02-25 18:59:29 -0300

    observables and promise

--- section_2/adminpro/src/app/pages/pages-routing.module.ts
+++ section_2/adminpro/src/app/pages/pages-routing.module.ts
@@ -6,6 +6,8 @@ import { ProgressComponent } from './progress/progress.component';
 import { GraphicOneComponent } from './graphic-one/graphic-one.component';
 import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromisesComponent } from './promises/promises.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 const pagesRoutes: Routes = [
@@ -13,10 +15,12 @@ const pagesRoutes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-          {path: 'dashboard', component: DashboardComponent},
-          {path: 'progress', component: ProgressComponent},
-          {path: 'graphic', component: GraphicOneComponent},
-          {path: 'settings', component: AccountSettingsComponent},
+          {path: 'dashboard', component: DashboardComponent, data: {title: 'Dashboard'}},
+          {path: 'progress', component: ProgressComponent, data: {title: 'Progress'}},
+          {path: 'graphic', component: GraphicOneComponent, data: {title: 'Graphic'}},
+          {path: 'settings', component: AccountSettingsComponent, data: {title: 'Settings'}},
+          {path: 'promise', component: PromisesComponent, data: {title: 'Promise'}},
+          {path: 'rxjs', component: RxjsComponent, data: {title: 'RXJS'}},
           {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
         ]
       }

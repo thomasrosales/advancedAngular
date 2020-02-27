65775c197f87e731c9a6a07504fd2ce077f45c9f
thomasrosales <tomasjoaquinrosales@gmail.com>
2020-02-27 10:02:49 -0300

    Google Singin with LiteServer

--- section_2/backend-server/config/config.js
+++ section_2/backend-server/config/config.js
@@ -1,1 +1,4 @@
 module.exports.SECRET_KEY = '@SECRET-KEY';
+
+// GOOGLE CREDENTIALS
+module.exports.GOOGLE_CLIENT_ID = '738475857891-8eqqhau2c83o1mknk68020ih9iah2aia.apps.googleusercontent.com';
--- section_2/backend-server/routes/login.js
+++ section_2/backend-server/routes/login.js
@@ -4,6 +4,9 @@ var bcrypt = require('bcrypt');
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var SECRET_KEY = require('../config/config').SECRET_KEY;
+var GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 /**
  * LOGIN
@@ -51,6 +54,58 @@ app.post('/', (req, res) => {
         }); // EVERYTHING OK
     });
 });
+
+/**
+ * GOOGLE AUTHENTICATION
+ */
+app.post('/google', async(req, res, next) => {
+
+    var token = req.body.token;
+
+    if (!token) {
+        return res.status(400).json({
+            status: false,
+            description: 'Token is required.', //SACAR
+            errors: {}
+        });
+    }
+
+    var googleUser = await verify(token).catch(err => {
+        return res.status(403).json({
+            status: false,
+            description: 'Token Invalid', //SACAR
+            errors: err
+        });
+    });
+
+
+    res.status(200).json({
+        status: true,
+        description: 'OK',
+        googleUser: googleUser
+    }); // EVERYTHING OK
+
+});
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: GOOGLE_CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        name: payload.name,
+        email: payload.email,
+        image: payload.picture,
+        google: true
+    }
+}
 
 
 module.exports = app;
